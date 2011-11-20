e331d117872c7657ecab7a6685a53f96914a53a8
Gianpaulo Soares <jam_pow@hotmail.com>
2011-11-20 17:58:51 -0200

    adicionando aviso de inicialização ao servidor

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var connect = require('connect');
 
 var server = connect(
   connect.static(__dirname)
-);
+).listen(8000);
 
-server.listen(8000);
+console.log('Servidor iniciado: http://localhost:8000');
 
