18e3438502a42a29345c87fc8cb283cfd6244fd1
Gianpaulo Soares <jam_pow@hotmail.com>
2011-11-21 21:26:04 -0200

    adicionando log ao servidor de testes

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var connect = require('connect');
 
 var server = connect(
-  connect.static(__dirname)
+  connect.static(__dirname),
+  connect.logger()
 ).listen(8000);
 
 console.log('Servidor iniciado: http://localhost:8000');
